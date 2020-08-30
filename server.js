1f7840b368b9318ccbf375681a579957247b3d3b
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-08-30 14:47:48 -0700

    build folder

--- server.js
+++ server.js
@@ -83,3 +83,11 @@ db.sequelize
   .catch((err) => {
     throw err;
   });
+
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+  }
