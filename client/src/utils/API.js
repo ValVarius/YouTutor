5bab31eded0d5eed1b157bd4a39d494ed6162345
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-09-03 16:17:52 -0700

    fixed base_url

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL = "https://you-tutor-backend.herokuapp.com"
-const BASE_URL = "https://you-tutor.herokuapp.com/"
+const BASE_URL = "https://you-tutor.herokuapp.com"
 
 // const BASE_URL = "http://localhost:8080"
 const API = {
