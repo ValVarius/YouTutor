41037b5dfcdd27c9a3a17f8bbc969e1823732636
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-09-02 22:58:44 -0700

    wip

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 // const BASE_URL = "https://you-tutor-backend.herokuapp.com"
-const BASE_URL = "https://you-tutor.herokuapp.com/"
+// const BASE_URL = "https://you-tutor.herokuapp.com/"
 
-// const BASE_URL = "http://localhost:8080"
+const BASE_URL = "http://localhost:8080"
 const API = {
     login:function(userData){
         return axios.post(`${BASE_URL}/login`,userData,{withCredentials:true})
