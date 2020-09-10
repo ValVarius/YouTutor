75546c63ef79da0d13bb7716f997ff79bade7102
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-09-10 15:18:56 -0700

    loading function for deployment

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const BASE_URL = "https://you-tutor-backend.herokuapp.com"
 // const BASE_URL = "https://you-tutor.herokuapp.com"
-const BASE_URL = process.env.PUBLIC_URL || "http://localhost:8080"
+const BASE_URL = process.env.PUBLIC_URL // || "http://localhost:8080"
 
 // const BASE_URL = "http://localhost:8080"
 const API = {
