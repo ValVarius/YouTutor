b03ed57284ed673abbe8b323b7725c8343c15f41
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-09-10 14:43:46 -0700

    changed / route

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", function (req, res) {
+router.get("/Signup", function (req, res) {
   res.send("Welcome to the YouTutor API");
 });
 
