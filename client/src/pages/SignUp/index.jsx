4d675fcee8762641ffd9bbdefb9a643e1a31bde0
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-10-27 19:52:58 -0700

    new signup page

--- client/src/pages/SignUp/index.jsx
+++ client/src/pages/SignUp/index.jsx
@@ -7,11 +7,6 @@ import { Link } from "react-router-dom";
 // import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react';
 
 export default function SignUp(props) {
-  
-
-  
-
-
   const getPicture = (event) => {
     let Picture = event.target.files[0];
     console.log(Picture);
@@ -73,13 +68,13 @@ export default function SignUp(props) {
   };
 
   return (
-    <div className="UserForm">
+    <form className="UserForm ">
       <label className="label is-large">Sign Up:</label>
       <div className="field">
         <label className="label">First Name</label>
         <div className="control">
           <input
-            className="input"
+            className="input is-rounded"
             type="text"
             onChange={handleInputChange}
             name="first_name"
@@ -93,7 +88,7 @@ export default function SignUp(props) {
         <label className="label">Last Name</label>
         <div className="control">
           <input
-            className="input"
+            className="input is-rounded"
             type="text"
             onChange={handleInputChange}
             name="last_name"
@@ -119,56 +114,87 @@ export default function SignUp(props) {
 
       <div className="field">
         <label className="label">Email</label>
-        <div className="control">
+        <p className="control has-icons-left">
           <input
-            className="input"
-            type="text"
+            className="input is-rounded"
+            type="email"
+            placeholder="Email"
             onChange={handleInputChange}
             name="email"
             value={userState.email || ""}
-            placeholder="Email"
           />
-        </div>
+          <span className="icon is-small is-left">
+            <i className="fas fa-envelope"></i>
+          </span>
+        </p>
       </div>
 
       <div className="field">
         <label className="label">Password</label>
-        <div className="control">
+        <p className="control has-icons-left">
           <input
             type="password"
-            className="input"
+            className="input is-rounded"
             onChange={handleInputChange}
             name="password"
             value={userState.password || ""}
             placeholder="Password"
           />
-        </div>
+          <span className="icon is-small is-left">
+            <i className="fas fa-lock"></i>
+          </span>
+        </p>
       </div>
 
       {/* <ImageUpload getPicture= {getPicture}/> */}
       <br />
-      <input type="file" onChange={getPicture} />
+
+      <div class="file has-name is-warning is-centered">
+        <label class="file-label">
+          <input
+            class="file-input"
+            type="file"
+            name="profile"
+            onChange={getPicture}
+          />
+          <span class="file-cta">
+            <span class="file-icon">
+              <i class="fas fa-upload"></i>
+            </span>
+            <span class="file-label">Choose a Profile Picture</span>
+            <span class="file-name">{userState.picture}</span>
+          </span>
+        </label>
+      </div>
+
       <br />
-      <br />
-      <br />
+
       <div className="card-image" id="previewImg">
-        <figure className="image is-4by3">
-          <img src={props.BASE_URL + userState.picture} alt={userState.picture} />
+        <figure className="image is-4by5">
+          <img
+            src={props.BASE_URL + userState.picture}
+            alt={userState.picture}
+          />
         </figure>
       </div>
+
       <br />
-      <div className="field is-grouped">
-        <div className="control">
-          <button className="button is-link" onClick={handleFormSubmit}>
-            {/* <Link to ='/profile'>Submit</Link> */} Submit
-          </button>
-        </div>
-        <div className="control">
-          <button className="button is-link is-light">
-            <Link to="/">Cancel</Link>
-          </button>
+      <div className="buttons is-centered">
+        <div className="field is-grouped ">
+          <div className="control">
+            <button className="button is-link" onClick={handleFormSubmit}>
+              {/* <Link to ='/profile'>Submit</Link> */} Submit
+            </button>
+          </div>
+          <div className="control">
+            <button className="button is-link is-light">
+              <Link to="/">Cancel</Link>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+
+      <br />
+    </form>
   );
 }
